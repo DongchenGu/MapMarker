94a9ec62f1b7f69eb33d82449e4b653834537abf
Nobert <nobertdalton@gmail.com>
2022-06-16 15:46:19 -0400

    MapMarker

---
+++ module1/src/frame/MainFrame.java
@@ -0,0 +1,19 @@
+package frame;
+
+
+import processing.core.PApplet;
+
+import java.awt.*;
+
+public class MainFrame extends PApplet {
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
