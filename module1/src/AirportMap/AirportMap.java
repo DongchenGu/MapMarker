819bb62e39290ef4f94e5fa7dafef946240bf2ec
Nobert <nobertdalton@gmail.com>
2022-06-29 11:14:14 -0400

    update4

---
+++ module1/src/AirportMap/AirportMap.java
@@ -0,0 +1,75 @@
+package AirportMap;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.Microsoft;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import parsing.ParseFeed;
+import processing.core.PApplet;
+
+import javax.media.nativewindow.util.Point;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class AirportMap  extends PApplet {
+
+    UnfoldingMap map;
+    List<Marker> airportMarkers;
+    Marker lastSelected;
+
+    @Override
+    public void setup() {
+       size(1400,900,OPENGL);
+       map = new UnfoldingMap(this,0,40, 1400,860,new Microsoft.RoadProvider());
+       MapUtils.createDefaultEventDispatcher(this,map);
+
+       List<PointFeature> airportFeatures = ParseFeed.parseAirports(this,"L:\\MapMarker\\module1\\src\\data\\airports.dat");
+
+       airportMarkers = new ArrayList<>();
+       Iterator<PointFeature> it = airportFeatures.iterator();
+       while(it.hasNext()){
+          // System.out.println(it.next().getLocation());
+           AirportMarker airportMarker = new AirportMarker(it.next());
+           airportMarkers.add(airportMarker);
+       }
+       map.addMarkers(airportMarkers);
+       map.zoomAndPanTo(5, new Location(39.95f, -75.12f));
+    }
+
+
+    @Override
+    public void draw() {
+        map.draw();
+    }
+
+    @Override
+    public void mouseMoved() {
+        // clear the last selection
+        if (lastSelected != null) {
+            lastSelected.setSelected(false);
+            lastSelected = null;
+
+        }
+        selectMarkerIfHover(airportMarkers);
+    }
+
+
+    public void selectMarkerIfHover(List<Marker> airportMarkers){
+        if (lastSelected != null) {
+            return;
+        }
+        for(Marker mk :airportMarkers){
+            if(mk.isInside(map,mouseX,mouseY)){
+                lastSelected = (AirportMarker)mk;
+                lastSelected.setSelected(true);
+                return;
+            }
+
+        }
+    }
+}
---
+++ module1/src/AirportMap/AirportMarker.java
@@ -0,0 +1,66 @@
+package AirportMap;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+import javax.media.nativewindow.util.Point;
+import javax.xml.stream.Location;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AirportMarker extends SimplePointMarker {
+
+    private boolean selected;
+
+    public AirportMarker(PointFeature pointFeature){
+        super(pointFeature.getLocation(), pointFeature.getProperties());
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void draw(PGraphics pg, float x, float y) {
+        if (!hidden) {
+            drawMarker(pg, x, y);
+            if (selected) {
+                showTitle(pg, x, y);
+            }
+        }
+    }
+
+    public  void drawMarker(PGraphics pg, float x, float y){
+        pg.fill(122,35,125);
+        pg.triangle(x-5,y+5,x,y-5,x+5,y+5);
+
+    }
+
+    public  void showTitle(PGraphics pg, float x, float y){
+
+        String title = this.getStringProperty("code")+ " "+ this.getStringProperty("name")+" "
+               + this.getStringProperty("city");
+        //System.out.println(title);
+        pg.pushStyle();
+        pg.rectMode(PConstants.CORNER);
+
+        pg.stroke(110);
+        pg.fill(255,255,255);
+        pg.rect(x, y + 15, pg.textWidth(title) +6, 18, 5);
+
+        pg.textAlign(PConstants.LEFT, PConstants.TOP);
+        pg.fill(0);
+        pg.text(title, x + 3 , y +18);
+
+        pg.popStyle();
+       // pg.text(title,x,y);
+    }
+}
